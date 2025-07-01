35b281b6e2cb6cfcf001f0b06002fd6802e8692b
ericayanru-dev <ericayanru@outlook.com>
2025-07-01 14:31:08 +0100

    included a prompt message for user to input first and last name

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -4,6 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        Console.Write("What is your first name?");
+        string first_name = Console.ReadLine();
+        first_name = char.ToUpper(first_name[0]) + first_name.Substring(1);
+        Console.Write("What is your last name?");
+        string last_name = Console.ReadLine();
+        last_name = char.ToUpper(last_name[0]) + last_name.Substring(1);
+
+        Console.WriteLine($"Your name is {last_name}, {first_name} {last_name}.");
     }
 }
