588fdeb169ba1cc02946edd9bdd9cc5afb02e5a3
ericayanru-dev <ericayanru@outlook.com>
2025-07-02 21:45:51 +0100

    program that has several simple functions

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -5,5 +5,36 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise5 Project.");
+        static void DisplayWelcome()
+        {
+            Console.WriteLine("Welcome to the program!");
+        }
+        static string PromptUserName()
+        {
+            Console.Write("Please enter your name: ");
+            string userName = Console.ReadLine();
+            return userName;
+        }
+        static int PromptUserNumber()
+        {
+            Console.Write("Please enter your favorite number: ");
+            int userNumber = int.Parse(Console.ReadLine());
+            return userNumber;
+        }
+        static double SquareNumber(int number)
+        {
+            double squareNumber = number * number;
+            return squareNumber;
+        }
+        static void DisplayResult(string userName, double square)
+        {
+            Console.WriteLine($"{userName}, the square of your number is {square}");
+        }
+
+        DisplayWelcome();
+        string userName = PromptUserName();
+        int userNumber = PromptUserNumber();
+        double square = SquareNumber(userNumber);
+        DisplayResult(userName, square);
     }
 }
