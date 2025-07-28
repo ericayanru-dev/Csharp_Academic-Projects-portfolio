6e3db67bb5514050804f751b02b505e37292c527
ericayanru-dev <ericayanru@outlook.com>
2025-07-28 18:39:13 +0100

    Inheritance

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -5,5 +5,15 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Homework Project.");
+        Assignment assignment = new Assignment("Samuel Bennett", "Multiplication");
+        assignment.GetSummary();
+
+        MathAssignment mathAssignment = new MathAssignment("Roberto Rodriguez", "Fractions", "Section 7.3", "Problems 8-19");
+        mathAssignment.GetSummary();
+        mathAssignment.GetHomeworkList();
+
+        WritingAssignment writingAssignment = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II");
+        writingAssignment.GetSummary();
+        writingAssignment.GetWritingInformation();
     }
 }
